d8c82b575e26ba4a85836fd427a2ef24e1bef74a
GuiltyWhite <yaroslav01593@gmail.com>
2018-01-08 21:09:45 +0200

    fix

--- pin.js
+++ pin.js
@@ -1,19 +1,17 @@
-let pinCheckerInit = (attempt = 1) => {
+let pinCheckerInit = (maxAttempts, attempt = 1) => {
     let input = prompt('Enter pin (4 digits)', '0000');
-
     let pinChecker = (input) => {
         const pin = 1234;
         if (input == pin) {
             console.log('Access granted');
             return true
         } else {
-            console.log(`Incorrect pin, attempts left ${4 - attempt}`);
+            console.log(`Incorrect pin, attempts left ${maxAttempts - attempt}`);
             attempt++;
-            pinCheckerInit(attempt);
+            pinCheckerInit(maxAttempts, attempt);
         }
     };
-
-    if (attempt < 4) {
+    if (attempt < maxAttempts) {
         pinChecker(input)
     } else {
         console.log('Access denied. All attempts were used');
@@ -22,4 +20,4 @@ let pinCheckerInit = (attempt = 1) => {
 
 };
 
-pinCheckerInit();
+pinCheckerInit(5);
